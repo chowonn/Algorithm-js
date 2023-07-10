13919968b07884af4225548c8132808dfe969f14
chowonn <70478015+chowonn@users.noreply.github.com>
2023-07-11 01:05:34 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let answer = 0;
+    for(let i=1; i<=100; i++){
+         if(n%7==0){
+        return answer = Math.floor(n/7);
+    }else{
+       return answer = Math.floor(n/7) + 1;
+    }}
+       
+}
