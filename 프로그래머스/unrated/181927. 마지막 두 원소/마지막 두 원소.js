334d1eff5be0fa931bf964a168b41766ce205509
chowonn <70478015+chowonn@users.noreply.github.com>
2023-07-25 02:28:18 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let num1 = num_list[num_list.length-1];
+    let num2 = num_list[num_list.length-2];
+    if(num1 > num2){
+        num_list.push(num1-num2);
+    }else{
+        num_list.push(num1*2);
+    }
+    return num_list;
+}
