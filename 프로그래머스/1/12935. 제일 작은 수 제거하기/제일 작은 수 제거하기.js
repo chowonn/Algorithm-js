ca0a63175a0a0be3f5c15d721e314689f00d58cc
chowonn <70478015+chowonn@users.noreply.github.com>
2023-11-24 20:09:44 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,7 @@
+function solution(arr) {
+    const smallNum = Math.min(...arr);
+    const answer = arr.splice(arr.indexOf(smallNum),1);
+    const result = arr.length ? arr : [-1];
+    
+    return result;
+}
