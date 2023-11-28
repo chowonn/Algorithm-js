6691472b787cd2378c546773686e77894765c432
chowonn <70478015+chowonn@users.noreply.github.com>
2023-11-28 11:40:14 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,18 @@
+// function solution(absolutes, signs) {
+//     let answer = 0;
+//     for(let i=0; i<absolutes.length; i++){
+//         if(signs[i] === false){
+//             answer += -absolutes[i];
+//         }else{
+//             answer += absolutes[i];
+//         }
+//     }
+//     return answer;
+// }
+
 function solution(absolutes, signs) {
-    let answer = 0;
-    for(let i=0; i<absolutes.length; i++){
-        if(signs[i] === false){
-            answer += -absolutes[i];
-        }else{
-            answer += absolutes[i];
-        }
-    }
-    return answer;
+
+    return absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
 }
+ 
+                                  
