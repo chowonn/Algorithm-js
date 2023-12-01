5b522d65959c2e778479a90c26b8ba74cfbd8217
chowonn <70478015+chowonn@users.noreply.github.com>
2023-12-01 21:52:16 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,17 +1,29 @@
-function solution(num) {
-    let answer = 0;
-    let i=1;
-   if(num===1){
-       return 0;
-   }
+// function solution(num) {
+//     let answer = 0;
+//     let i=1;
+//    if(num===1){
+//        return 0;
+//    }
     
-    while(i<501){
-        num = num%2===0 ? num/2 : num*3+1;
-        if(num===1){
-            return i;
-        }else{
-            i+=1;
+//     while(i<501){
+//         num = num%2===0 ? num/2 : num*3+1;
+//         if(num===1){
+//             return i;
+//         }else{
+//             i+=1;
+//         }
+//     }
+//     return -1;
+// }
+
+function solution(a){
+        let count=0;
+        function cola(b){
+            if(count > 499) return -1;
+            if(b === 1) return count;
+            count++;
+            b%2 === 0 ? cola(b/2) : cola((b*3)+1)
         }
+        cola(a)
+        return count > 499 ? -1 : count;
     }
-    return -1;
-}
