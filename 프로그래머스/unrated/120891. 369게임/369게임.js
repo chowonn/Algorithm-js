34f40e0d5e4cff40387a6e86cc93b07910b80342
chowonn <70478015+chowonn@users.noreply.github.com>
2024-01-08 22:47:09 +0900

    [level unrated] Title: 369게임, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120891. 369게임/369게임.js
+++ 프로그래머스/unrated/120891. 369게임/369게임.js
@@ -1,6 +1,13 @@
+// function solution(order) {
+//     const num = ['3','6','9'];
+//     const answer = String(order).split('').filter((a)=> num.includes(a)).length;
+//     console.log(answer);
+//     return answer;
+// }
+
 function solution(order) {
-    const num = ['3','6','9'];
-    const answer = String(order).split('').filter((a)=> num.includes(a)).length;
-    console.log(answer);
-    return answer;
+    const mySet = new Set([3,6,9]);
+    return String(order).split('')
+                        .filter(num => mySet.has(Number(num)))
+                        .length;
 }
