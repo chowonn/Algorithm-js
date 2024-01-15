7cc29e90459ce582b9e59642edbaaf7b63162272
chowonn <70478015+chowonn@users.noreply.github.com>
2024-01-16 00:13:32 +0900

    [level Lv.0] Title: 글자 지우기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/Lv.0/181900. 글자 지우기/글자 지우기.js
@@ -1,5 +1,10 @@
+// function solution(my_string, indices) {
+//     let answer = [...my_string].filter((_,idx)=> !indices.includes(idx)).join('');
+//     console.log(answer);
+//     return answer;
+// }
+
 function solution(my_string, indices) {
-    let answer = [...my_string].filter((_,idx)=> !indices.includes(idx)).join('');
-    console.log(answer);
-    return answer;
+  const set = new Set(indices);
+  return [...my_string].filter((_, i) => !set.has(i)).join('');
 }
